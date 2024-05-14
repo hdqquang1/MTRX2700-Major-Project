41bc5358150677be786ec97db77d3339694898ef
omar-abdulhalim <160563340+omar-abdulhalim@users.noreply.github.com>
2024-05-14 15:39:32 +1000

    whatever

--- Major Project/Core/Src/main.c
+++ Major Project/Core/Src/main.c
@@ -56,33 +56,36 @@ void set_LEDs(){
 int main(void)
 {
 	initialise_board();
-
 	SerialInitialise(BAUD_115200, &USART1_PORT, 0x00);
 	uint8_t string_to_send[64] = "This is a string !\r\n";
 
 	float gyro_values[3];
 
+	uint8_t rounds = 1;
 	uint8_t outcome;
+	uint8_t winner;
+	uint8_t finalWinner;
+	uint8_t multiplier;
+	uint8_t P1leaderboard;
+	uint8_t P2leaderboard;
 
 	srand(time(NULL));  // Seed the random number generator once
 
-	while (1){
+	while (rounds <= 3){
+
+		uint8_t P1score = 0;
+		uint8_t P2score = 0;
 
 		trigger_oneshot(30000, &set_timesUp);
-		sprintf(string_to_send, "Timer starts now!\r\n");
+		sprintf(string_to_send, "Player 1 your timer starts now!\r\n");
 		SerialOutputString(string_to_send, &USART1_PORT);
 
-		uint8_t score = 0;
-
 		while (timesUp == 0) {
-
 			outcome = 0;
-
 
 			uint8_t guessWord[20];
 			char* random = randomWord();
 			strcpy((char*)guessWord, random);
-
 			sprintf(string_to_send, "%s\r\n", (char*)guessWord);
 			SerialOutputString(string_to_send, &USART1_PORT);
 
@@ -98,7 +101,7 @@ int main(void)
 			}
 
 			if (outcome == 1){
-				score++;
+				P1score++;
 				sprintf(string_to_send, "Correct!\r\n");
 				SerialOutputString(string_to_send, &USART1_PORT);
 				flash_leds();
@@ -109,10 +112,91 @@ int main(void)
 			}
 			HAL_Delay(750);
 		}
+		sprintf(string_to_send, "Time's Up! Player 1's score is %d!\r\n", P1score);
+		SerialOutputString(string_to_send, &USART1_PORT);
+		set_timesUp();
 
-		sprintf(string_to_send, "Time's Up! Your score is %d!\r\n", score);
+		// Player 2 plays heads up
+		trigger_oneshot(30000, &set_timesUp);
+		sprintf(string_to_send, "Player 2 your timer starts now!\r\n");
 		SerialOutputString(string_to_send, &USART1_PORT);
 
-		for(;;){}
+		while (timesUp == 0) {
+			outcome = 0;
+
+			uint8_t guessWord[20];
+			char* random = randomWord();
+			strcpy((char*)guessWord, random);
+			sprintf(string_to_send, "%s\r\n", (char*)guessWord);
+			SerialOutputString(string_to_send, &USART1_PORT);
+
+			while (!outcome){
+				BSP_GYRO_GetXYZ(&gyro_values[0]);
+				if (gyro_values[1]/20000 >= 25){
+					outcome = 1;
+				}
+				if (gyro_values[1]/20000 <= -25){
+					outcome = 2;
+				}
+				HAL_Delay(100);
+			}
+
+			if (outcome == 1){
+				P2score++;
+				sprintf(string_to_send, "Correct!\r\n");
+				SerialOutputString(string_to_send, &USART1_PORT);
+				flash_leds();
+			}
+			else if (outcome == 2){
+				sprintf(string_to_send, "Pass!\r\n");
+				SerialOutputString(string_to_send, &USART1_PORT);
+			}
+			HAL_Delay(750);
+		}
+		sprintf(string_to_send, "Time's Up! Player 2's score is %d!\r\n", P2score);
+		SerialOutputString(string_to_send, &USART1_PORT);
+		set_timesUp();
+
+
+		// Winner determined
+		if (P1score > P2score){
+			// player 1 will play wavelength
+			winner = 1;
+		}
+		else if (P2score > P1score){
+			// player 2 will play wavelength
+			winner = 2;
+		}
+
+		// Multiplier
+		multiplier = 2;
+
+		// Wavelength multiplier
+		if (winner == 1){
+			P1score = P1score * multiplier;
+		}
+		else if (winner == 2){
+			P2score = P2score * multiplier;
+		}
+
+		// Update leaderboard
+		P1leaderboard = P1leaderboard + P1score;
+		P2leaderboard = P2leaderboard + P2score;
+		sprintf(string_to_send, "Player 1 your total score is %d!\r\n", P1leaderboard);
+		SerialOutputString(string_to_send, &USART1_PORT);
+		sprintf(string_to_send, "Player 2 your total score is %d!\r\n", P2leaderboard);
+		SerialOutputString(string_to_send, &USART1_PORT);
+
+		rounds++;
+	}
+
+	// Final winner announced
+	if (P1leaderboard > P2leaderboard){
+		sprintf(string_to_send, "Player 1 Wins with a final score of %d!\r\n", P1leaderboard);
+		SerialOutputString(string_to_send, &USART1_PORT);
+	}
+	else if (P2leaderboard > P1leaderboard){
+		sprintf(string_to_send, "Player 2 Wins with a final score of %d!\r\n", P2leaderboard);
+		SerialOutputString(string_to_send, &USART1_PORT);
 	}
 }

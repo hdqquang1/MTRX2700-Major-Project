9d6b09705195f13fe96611633870b92ab92c83f7
hdqquang1 <160237143+hdqquang1@users.noreply.github.com>
2024-05-19 12:38:57 +1000

    Complete Heads Up

---
+++ Major Project/Core/Inc/digital_io.h
@@ -0,0 +1,13 @@
+#ifndef DIGITAL_IO_H
+#define DIGITAL_IO_H
+
+// correct_leds(): turn on LEDs to create a circle for 1s
+void correct_leds();
+
+// incorrect_leds(): turn on LEDs to create a cross for 1s
+void incorrect_leds();
+
+// end_leds(): turn on LEDs to create a circle for 5s
+void end_leds();
+
+#endif
---
+++ Major Project/Debug/Core/Src/digital_io.d
@@ -0,0 +1,18 @@
+Core/Src/digital_io.o: ../Core/Src/digital_io.c ../Core/Inc/digital_io.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ ../Core/Inc/timers.h
+../Core/Inc/digital_io.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+../Core/Inc/timers.h:

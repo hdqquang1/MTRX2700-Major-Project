5ad910809302c2166f7226f467c277240e7f733a
hdqquang1 <160237143+hdqquang1@users.noreply.github.com>
2024-05-21 14:25:12 +1000

    Final code

--- Major Project/Core/Src/digital_io.c
+++ Major Project/Core/Src/digital_io.c
@@ -10,6 +10,8 @@
 #define INCORRECT 0b01010101
 
 
+// get_button_state - Get button state
+// Returns button state
 uint8_t get_button_state(void){
 	return GPIOA->IDR & 0x01;
 }
@@ -27,11 +29,17 @@ void set_leds(uint8_t led_mask_pattern)
     }
 }
 
+
+// get_leds - Get LEDs current state
+// Returns LEDs current state
 uint8_t get_leds(void){
     uint8_t *led_register = ((uint8_t*)&(GPIOE->ODR)) + 1;
     return *led_register;
 }
 
+
+// non_blocking_delay - Basic delay that doesn't stop game status
+// Input: delay period in millisecond
 void non_blocking_delay(uint32_t delay_ms) {
     uint32_t start_tick = HAL_GetTick();
     while (HAL_GetTick() - start_tick < delay_ms) {
@@ -40,6 +48,8 @@ void non_blocking_delay(uint32_t delay_ms) {
     }
 }
 
+
+// correct_leds - Turn on circle LEDs pattern for 1s
 void correct_leds(void){
     // Turn on LEDs
     set_leds(CORRECT);
@@ -51,6 +61,8 @@ void correct_leds(void){
     set_leds(0);
 }
 
+
+// incorrect_leds - Turn on cross LEDs pattern for 1s
 void incorrect_leds(void){
     // Turn on LEDs
     set_leds(INCORRECT);
@@ -62,6 +74,8 @@ void incorrect_leds(void){
     set_leds(0);
 }
 
+
+// end_leds - Turn on circle LEDs pattern for 2s
 void end_leds(void){
     // Turn on LEDs
     set_leds(CORRECT);
@@ -73,6 +87,8 @@ void end_leds(void){
     set_leds(0);
 }
 
+
+// chase_leds - Shift LED 1 by 1
 void chase_leds(void){
     uint8_t mask = get_leds() << 1;
     set_leds(mask);
